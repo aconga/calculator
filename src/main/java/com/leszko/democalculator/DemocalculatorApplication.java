91d9cabe90b3ac1936e3c8b843ff079bdb383cfd
aconga <acongacardenas@gmail.com>
2018-12-20 16:58:40 -0500

    Add Spring Boot skeleton

---
+++ src/main/java/com/leszko/democalculator/DemocalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.leszko.democalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocalculatorApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/leszko/democalculator/DemocalculatorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leszko.democalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemocalculatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
